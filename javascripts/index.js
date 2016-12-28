33775e19cbe0bf7a4184ae0faa1f346f815736f5
alangranadeiro <alangranadeiro@gmail.com>
2016-12-28 11:59:52 -0200

    - correção no formulário - inclusão do ajax/post

---
+++ javascripts/index.js
@@ -0,0 +1,25 @@
+$(function() {
+
+	//Click do btEnvia
+	$('#btEnvia').on('click',function() {
+		var data = {
+		    name: $("nome").val(),
+		    email: $("email").val(),
+		    message: $("mensagem").val()
+		};
+		//Ajax para envio do formulário
+		$.ajax({ 
+			type: "POST",
+			url: "mail/envia.php",
+			data: data,
+			success: function(){
+				$('#msgEmail')[0].innerText = "Enviado com sucesso!";
+			},
+			error: function(){
+				$('#msgEmail')[0].innerText = "Erro ao enviar email. Por favor, tente mais tarde.";
+			}
+		});
+	    return false;
+	});
+	
+});
