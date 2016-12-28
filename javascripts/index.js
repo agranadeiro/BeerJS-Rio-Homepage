00b7446325f006283893402b93487a04971c0e92
alangranadeiro <alangranadeiro@gmail.com>
2016-12-28 14:48:52 -0200

    - ajustes ajax formulário

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,25 +1,36 @@
 $(function() {
 
+	//iniciando div msgEmail escondida 
+	$('#msgEmail').css('display','none');
+	
 	//Click do btEnvia
 	$('#btEnviar').on('click',function() {
 		var data = {
-		    name: $("nome").val(),
-		    email: $("email").val(),
-		    message: $("mensagem").val()
+		    name: $('nome').val(),
+		    email: $('email').val(),
+		    message: $('mensagem').val()
 		};
 		//Ajax para envio do formulário
 		$.ajax({ 
 			type: "POST",
 			url: "mail/envia.php",
 			data: data,
-			success: function(){
-				$('#msgEmail')[0].innerText = "Enviado com sucesso!";
+			success: function(){			
+				$('#msgEmail').innerText = "Enviado com sucesso!";
+				$('#msgEmail').attr('alert-success','alert-success')
+				$('nome').val("");
+				$('email').val("");
+				$('mensagem').val(""); 
 			},
 			error: function(){
-				$('#msgEmail')[0].innerText = "Erro ao enviar email. Por favor, tente mais tarde.";
+				$('#msgEmail').innerText = "Erro ao enviar email. Por favor, tente mais tarde.";
+				$('#msgEmail').attr('alert-success','alert-danger')			
 			}
 		});
-	    return false;
+		
+		$('#msgEmail').css('display','block');
+	    
+		return false;
 	});
 	
 });
