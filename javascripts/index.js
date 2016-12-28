690c81856192f897f49696aa311f64336009a5ac
alangranadeiro <alangranadeiro@gmail.com>
2016-12-28 17:22:07 -0200

    - inclusão da validação dos campos do formulário de contato

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,7 +1,10 @@
 $(function() {
 
-	//Iniciando div/alert msgEmail escondida 
-	$('#msgEmail').css('display','none');
+	//Click do btContato
+	$('#btContato').on('click',function() {
+		$('#msgEmail').css('display','none');
+		$('#modalContato').modal('show');
+	});
 	
 	//Click do btEnvia
 	$('#btEnviar').on('click',function() {
@@ -11,22 +14,38 @@ $(function() {
 		}
 	});	
 
+	
 	//Validação dos campos do formulário
 	function validaCampos(){
 		
+		var retorno;
+		var emailFilter=/^.+@.+\..{2,}$/;
+		var illegalChars= /[\(\)\<\>\,\;\:\\\/\"\[\]]/;
+		var nome = $('#nome').val();
+		var email = $('#email').val();
+		var mensagem = $('#mensagem').val();
+				
 		//Verifica se tem campos em brancos
-    	if($('#nome').val() == "" || $('#email').val() == "" || $('#mensagem').val() == ""){
+    	if(nome == "" || email == "" || mensagem == ""){
     		
     	 	$('#msgEmail')[0].innerText = "O preenchimento de todos os campos é obrigatório.";
 			$('#msgEmail').attr('class','alert alert-danger');  
 			$('#msgEmail').css('display','block');
-    		return false;  	
+    		retorno = false;  	
     		
-    	} else { 		
-    		return true;
+    	} else if(!(emailFilter.test(email))||email.match(illegalChars)){ //Verificando email
+    		$('#msgEmail')[0].innerText = "Por favor, digite um email válido.";
+    		$('#msgEmail').css('display','block');
+    		retorno = false;
+    	
+    	} else { //Tudo ok
+    		retorno =  true;
     	}
+    	
+    	return retorno;
 	}
 
+	
 	//Envio do email
 	function enviaEmailContato(){
 		
